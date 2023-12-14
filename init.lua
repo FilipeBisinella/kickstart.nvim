f8c85d72ce482392cbcdfbea2659334aaa2b6ac0
Filipe Bisinella <filipe1.bs@gmail.com>
2023-12-14 16:20:17 -0300

    warmer theme

--- init.lua
+++ init.lua
@@ -194,6 +194,7 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup({ style = 'warmer' })
       vim.cmd.colorscheme 'onedark'
     end,
   },
