828e64838bc797cdbeff488c9e7f9aec282704d5
Filipe Bisinella <filipe1.bs@gmail.com>
2024-03-21 19:37:07 -0300

    scrollbar

--- init.lua
+++ init.lua
@@ -76,6 +76,8 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  'petertriho/nvim-scrollbar',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -209,6 +211,22 @@ require('lazy').setup({
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
       },
     },
   },
@@ -361,6 +379,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+require("scrollbar").setup({
+  handle = {
+    blend = 50,
+    color = "white",
+  },
+  marks = {
+    Cursor = {
+      text = " ",
+    }
+  }
 })
 
 -- [[ Configure Telescope ]]
