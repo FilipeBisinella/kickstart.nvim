1e17971428554c1d8d365f5608ca5462a7bb9df9
Filipe Bisinella <filipe1.bs@gmail.com>
2024-04-24 15:51:41 -0300

    lsp

--- init.lua
+++ init.lua
@@ -734,6 +734,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        gopls = {},
         pyright = {
           settings = {
             pyright = { -- Using Ruff's import organizer
