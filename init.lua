455c17fc009a2339f44da1eee8361e71019eef77
Filipe Bisinella <filipe1.bs@gmail.com>
2024-04-24 14:34:58 -0300

    json formatting

--- init.lua
+++ init.lua
@@ -758,6 +758,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'ruff_format', 'ruff_imports' },
+        json = { 'jq' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
