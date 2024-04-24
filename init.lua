310ec27647ce531980b0c68a7dc06e83f9bf0f23
Filipe Bisinella <filipe1.bs@gmail.com>
2024-04-24 15:50:28 -0300

    which keys

--- init.lua
+++ init.lua
@@ -322,7 +322,10 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        -- TODO
+        -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = '[X] Trouble', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
