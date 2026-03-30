1601aeb880c93187660312f0c1222f13bfd977b9
Filipe Bisinella <filipe1.bs@gmail.com>
2026-03-30 16:31:32 -0300

    lsp autogroups

--- init.lua
+++ init.lua
@@ -653,6 +653,10 @@ do
   -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
   -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
+  -- Create augroups once, outside the LspAttach callback
+  local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = true })
+  local detach_augroup = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true })
+
   -- Useful status updates for LSP.
   vim.pack.add { gh 'j-hui/fidget.nvim' }
   require('fidget').setup {}
@@ -691,9 +695,8 @@ do
       --    See `:help CursorHold` for information about when this is executed
       --
       -- When you move your cursor, the highlights will be cleared (the second autocommand).
-      local client = vim.lsp.get_client_by_id(event.data.client_id)
+      local client = vim.lsp.get_clients({ id = event.data.client_id })[1]
       if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-        local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
           buffer = event.buf,
           group = highlight_augroup,
@@ -707,7 +710,8 @@ do
         })
 
         vim.api.nvim_create_autocmd('LspDetach', {
-          group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+          buffer = event.buf,
+          group = detach_augroup,
           callback = function(event2)
             vim.lsp.buf.clear_references()
             vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
