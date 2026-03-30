336ad4cde2af9c5c53dac0a622b3dc2ab90cb2b2
Filipe Bisinella <filipe1.bs@gmail.com>
2026-03-30 17:30:53 -0300

    diagnostics

--- init.lua
+++ init.lua
@@ -173,11 +173,27 @@ do
   vim.diagnostic.config {
     update_in_insert = false,
     severity_sort = true,
-    float = { border = 'rounded', source = 'if_many' },
-    underline = { severity = { min = vim.diagnostic.severity.WARN } },
+    float = {
+      border = 'rounded',
+      source = 'if_many',
+      format = function(diagnostic)
+        if diagnostic.code then return string.format('%s (%s: %s)', diagnostic.message, diagnostic.source, diagnostic.code) end
+        if diagnostic.source then return string.format('%s (%s)', diagnostic.message, diagnostic.source) end
+        return diagnostic.message
+      end,
+    },
+    underline = { severity = { min = vim.diagnostic.severity.ERROR } },
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = 'E',
+        [vim.diagnostic.severity.WARN] = 'W',
+        [vim.diagnostic.severity.INFO] = 'I',
+        [vim.diagnostic.severity.HINT] = 'H',
+      },
+    },
 
     -- Can switch between these as you prefer
-    virtual_text = true, -- Text shows up at the end of the line
+    virtual_text = { severity = { min = vim.diagnostic.severity.WARN }, source = 'if_many', spacing = 2 }, -- Text shows up at the end of the line
     virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
