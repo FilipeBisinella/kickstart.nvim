fe32b66fb137349054842467649eb3e260f27c8f
Filipe Bisinella <filipe1.bs@gmail.com>
2026-04-06 19:02:58 -0300

    format python on save

--- init.lua
+++ init.lua
@@ -689,7 +689,7 @@ require('lazy').setup({
         -- You can specify filetypes to autoformat on save here:
         local enabled_filetypes = {
           -- lua = true,
-          -- python = true,
+          python = true,
         }
         if enabled_filetypes[vim.bo[bufnr].filetype] then
           return { timeout_ms = 500 }
